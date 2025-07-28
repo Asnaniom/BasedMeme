4860816669139eef3e269c848223513d657ea474
Asnaniom <98123962+Asnaniom@users.noreply.github.com>
2025-07-28 22:40:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/019851f5-f843-5d6c-bd73-2ad5d0445296", // <-- replace with your hosted manifest link if changed!
+        permanent: false // 307 (temporary redirect)
+      }
+    ];
+  },
+};
 
 export default nextConfig;
